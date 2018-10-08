f8e454faa3d5417777b01f9f198fb03187f0e7bd
Sam Griffiths <13700330+sjgriffiths@users.noreply.github.com>
2018-10-08 20:42:13 +0100

    Added Eigen and type aliases

--- Qlay/Core.h
+++ Qlay/Core.h
@@ -12,9 +12,24 @@
 
 #include <random>
 #include <chrono>
+#include <complex>
+
+#include <Eigen/Dense>
 
 namespace qlay
 {
 	//Global RNG used to simulate nondeterminism
 	extern std::default_random_engine rng;
+
+	//Complex number
+	using Complex = std::complex<double>;
+
+	//Bra (row) vector
+	using Bra = Eigen::Matrix<Complex, 1, Eigen::Dynamic>;
+
+	//Ket (column) vector
+	using Ket = Eigen::Matrix<Complex, Eigen::Dynamic, 1>;
+
+	//Generic matrix
+	using Mat = Eigen::Matrix<Complex, Eigen::Dynamic, Eigen::Dynamic>;
 }
