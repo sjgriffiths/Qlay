6bd1d207b95b90af39082d29c95135c9b3d51f05
Sam Griffiths <13700330+sjgriffiths@users.noreply.github.com>
2018-10-13 17:05:26 +0100

    Added global exception handler

--- QlayVisual/App.xaml.cs
+++ QlayVisual/App.xaml.cs
@@ -13,5 +13,10 @@ namespace QlayVisual
     /// </summary>
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Unhandled exception has occurred", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
     }
 }
