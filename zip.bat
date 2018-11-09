b965f2f9dd99875bdc260d1f70a22b9709745058
Sam Griffiths <13700330+sjgriffiths@users.noreply.github.com>
2018-11-09 01:02:51 +0000

    Fixed ZIP batch file to trim paths and run in background

--- zip.bat
+++ zip.bat
@@ -2,20 +2,24 @@
 
 REM Windows batch file to automatically zip files into release packages
 
-set "rar="C:\Program Files\WinRAR\winrar.exe""
+set "rar="C:\Program Files\WinRAR\winrar.exe" a -ibck -ep1 -afzip"
 
-%rar% a -afzip x86-Qlay.zip Qlay\Qlay.h
-%rar% a -afzip x86-Qlay.zip Release\Qlay.dll
-%rar% a -afzip x86-Qlay.zip Release\QlayCLI.dll
+%rar% x86-Qlay.zip Qlay\Qlay.h
+%rar% x86-Qlay.zip Release\Qlay.dll
+%rar% x86-Qlay.zip Release\Qlay.lib
+%rar% x86-Qlay.zip Release\QlayCLI.dll
 
-%rar% a -afzip x64-Qlay.zip Qlay\Qlay.h
-%rar% a -afzip x64-Qlay.zip x64\Release\Qlay.dll
-%rar% a -afzip x64-Qlay.zip x64\Release\QlayCLI.dll
+%rar% x64-Qlay.zip Qlay\Qlay.h
+%rar% x64-Qlay.zip x64\Release\Qlay.dll
+%rar% x64-Qlay.zip x64\Release\Qlay.lib
+%rar% x64-Qlay.zip x64\Release\QlayCLI.dll
 
-%rar% a -afzip x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayVisual.exe
-%rar% a -afzip x86-QlayVisual.zip QlayVisual\bin\x86\Release\Qlay.dll
-%rar% a -afzip x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayCLI.dll
+%rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayVisual.exe
+%rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\Qlay.dll
+%rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayCLI.dll
+%rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QLAYVISUAL.chm
 
-%rar% a -afzip x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayVisual.exe
-%rar% a -afzip x64-QlayVisual.zip QlayVisual\bin\x64\Release\Qlay.dll
-%rar% a -afzip x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayCLI.dll
+%rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayVisual.exe
+%rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\Qlay.dll
+%rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayCLI.dll
+%rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QLAYVISUAL.chm
