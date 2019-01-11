175d01c7d9f7d3a46cb2730598b18a8497e731ce
Sam Griffiths <13700330+sjgriffiths@users.noreply.github.com>
2019-01-11 20:38:30 +0000

    ZIP batch file now includes README.txt

--- zip.bat
+++ zip.bat
@@ -4,22 +4,30 @@ REM Windows batch file to automatically zip files into release packages
 
 set "rar="C:\Program Files\WinRAR\winrar.exe" a -ibck -ep1 -afzip"
 
+ren LICENSE.md LICENSE.txt
+
+%rar% x86-Qlay.zip LICENSE.txt
 %rar% x86-Qlay.zip Qlay\Qlay.h
 %rar% x86-Qlay.zip Release\Qlay.dll
 %rar% x86-Qlay.zip Release\Qlay.lib
 %rar% x86-Qlay.zip Release\QlayCLI.dll
 
+%rar% x64-Qlay.zip LICENSE.txt
 %rar% x64-Qlay.zip Qlay\Qlay.h
 %rar% x64-Qlay.zip x64\Release\Qlay.dll
 %rar% x64-Qlay.zip x64\Release\Qlay.lib
 %rar% x64-Qlay.zip x64\Release\QlayCLI.dll
 
+%rar% x86-QlayVisual.zip LICENSE.txt
 %rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayVisual.exe
 %rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\Qlay.dll
 %rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QlayCLI.dll
 %rar% x86-QlayVisual.zip QlayVisual\bin\x86\Release\QLAYVISUAL.chm
 
+%rar% x64-QlayVisual.zip LICENSE.txt
 %rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayVisual.exe
 %rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\Qlay.dll
 %rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QlayCLI.dll
 %rar% x64-QlayVisual.zip QlayVisual\bin\x64\Release\QLAYVISUAL.chm
+
+ren LICENSE.txt LICENSE.md
